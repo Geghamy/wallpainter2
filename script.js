f2de7dc0227e909589cff2eb366af2d7a9aadbd6
Geghamy <102752680+Geghamy@users.noreply.github.com>
2022-07-06 11:33:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let colors = [];
+
 fetch('https://627915ecd00bded55ae3f5c4.mockapi.io/api/v1/colors')
   .then((response) => {
     return response.json();
@@ -7,16 +9,21 @@ fetch('https://627915ecd00bded55ae3f5c4.mockapi.io/api/v1/colors')
   });
 
 const body = document.getElementsByTagName('body');
-let colors = [];
-let index = 0;
+const color = document.getElementById('color');
 
-color.innerHTML = colors;
-document.body.style.backgroundColor = colors;
+let index = 1;
+
+setTimeout(function () {
+  color.innerHTML = colors[0].code;
+  document.body.style.backgroundColor = colors[0].code;
+  console.log(color.innerHTML);
+}, 500);
+
 
 btn.addEventListener('click', function onClick() {
 
   document.body.style.backgroundColor = colors[index].code;
   color.innerHTML = colors[index].code;
-  
+
   index = index >= colors.length - 2 ? 0 : index + 1;
 });
